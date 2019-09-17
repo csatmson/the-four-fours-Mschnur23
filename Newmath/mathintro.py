5a17019a1110726d5fd969fb5044786baa5b6e08
Mschnur23 <55249832+Mschnur23@users.noreply.github.com>
2019-09-16 23:13:19 -0400

    Add files via upload

---
+++ Newmath/mathintro.py
@@ -0,0 +1,19 @@
+print("Zero is ", 4+4-4-4)
+
+print("One is ", 4*4//4//4)
+
+print("Two is ", (4**2-4-4)//4)
+
+print("Three is ", (4+4+4)//4)
+
+print("Four is ", 4**4//4//4//4 )
+
+print("Five is ", (4*4+4)//4)
+
+print("Six is ", (4+4)//4+4)
+
+print("Seven is ", 4+4-(4//4))
+
+print("Eight is ",(4+4)//4*4)
+
+print("Nine is ", 4+4+4//4)
